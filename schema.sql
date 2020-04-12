e21dd12b878675621e857f3dd39c6fa994406c58
Cwall4 <colin.wallace4@gmail.com>
2020-04-12 12:43:53 -0400

    Committing queries.sql.

--- schema.sql
+++ schema.sql
@@ -42,7 +42,7 @@ CREATE TABLE dept_emp (
   	to_date DATE NOT NULL,
   	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+  	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -51,7 +51,47 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, title, from_date, to_date)
 );
+-- Not sure if such a long pk is appropriate, but was running into duplicates when pk was just emp_no
+-- or (emp_no, title). This makes some sense, since an employee could, for example, be promoted, then demoted back to their old title.
 
-SELECT * FROM departments;
+-- SELECT * FROM departments;
+
+-- 7.3.1: 1952-1955 birth dates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Only 1952 birth dates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- 1953 birth dates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- 1954 birth dates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- 1955 birth dates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Narrowing the search
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
